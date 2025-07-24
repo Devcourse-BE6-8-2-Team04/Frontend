60006f8d9a7236c2a22b753c4b9eaa3ddf262fa6
DESKTOP-N5KD4EV\litte <littedeer108@gmail.com>
2025-07-24 10:29:31 +0900

    chore/빌드 설정

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-800">
+        글자색 배경색 바뀌면 tailwind 적용된 겁니다
+      </h1>
+    </div>
+  );
+}
