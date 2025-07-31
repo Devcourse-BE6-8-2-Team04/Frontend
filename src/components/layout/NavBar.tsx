4be743e1dd13df09315acfd63a922d1b5dcca050
DESKTOP-N5KD4EV\litte <littedeer108@gmail.com>
2025-07-31 16:29:47 +0900

    feat: navbar 경로 렌더링

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,13 +1,21 @@
-import React from "react";
-import { Home, Search, ShoppingBag, Menu } from "lucide-react";
+import Link from 'next/link';
+import { Search, Home, ShoppingBag, Menu } from 'lucide-react';
 
 export default function NavBar() {
   return (
     <div className="absolute w-[390px] h-[73px] bottom-0 left-0 bg-white flex justify-around items-center">
-      <Search className="w-6 h-6" />
-      <Home className="w-6 h-6" />
-      <ShoppingBag className="w-6 h-6" />
-      <Menu className="w-6 h-6" />
+      <Link href="/">
+        <Search className="w-6 h-6 cursor-pointer" />
+      </Link>
+      <Link href="/clothdetail">
+        <Home className="w-6 h-6 cursor-pointer" />
+      </Link>
+      <Link href="/clothdetail">
+        <ShoppingBag className="w-6 h-6 cursor-pointer" />
+      </Link>
+      <Link href="/plan">
+        <Menu className="w-6 h-6 cursor-pointer" />
+      </Link>
     </div>
   );
 }
